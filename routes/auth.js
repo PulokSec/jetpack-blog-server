476bc9904fb4638754bb36add9b088a7428aa52f
PulokSec <riopulok@gmial.com>
2021-06-30 19:13:49 +0600

    upload image

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,11 @@ const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPassword,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
@@ -23,11 +23,11 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username }||{ email: req.body.email});
-    !user && res.status(400).json("Wrong user!");
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong password!");
+    !validated && res.status(400).json("Wrong credentials!");
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
