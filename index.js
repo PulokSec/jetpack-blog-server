70281ea39898e0cc4b2bd33053818902164f8ada
PulokSec <riopulok@gmial.com>
2021-07-02 02:49:21 +0600

    remove multer

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
 const cors = require('cors');
 
 dotenv.config();
@@ -25,19 +24,7 @@ mongoose.connect(process.env.MONGO_URI,{
 app.get("/",(req,res)=>{
   res.send("Welcome to JETPACK Blog");
 })
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
 
-const upload = multer({ storage: storage });
-app.post("/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use("/auth", authRoute);
 app.use("/users", userRoute);
